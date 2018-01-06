eb7fe09137edc75eb5fe733846337dd42d5fd6db
BiancaMocan <mocan.bianca.maria@gmail.com>
2018-01-06 20:51:52 +0200

    add numere cu cifrele egale pana la 100

---
+++ Hello World/NumereCifreEgale/NumereCifreEgale/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumereCifreEgale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 100; i++)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    for(int k = 1; k <= i; k++)
+                    {
+                        Console.Write(j);
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
